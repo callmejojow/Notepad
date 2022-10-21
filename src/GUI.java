83c7574210ce35c344151a2876fcb4422b5428ff
callmejojow <73418982+callmejojow@users.noreply.github.com>
2022-10-21 18:31:53 -0400

    build a scroll bar, a text area and know some frames

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,70 @@
+import javax.swing.*;
+
 public class GUI {
+    JFrame window;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
+
+    public static void main(String[] args){
+        new GUI();
+    }
+    public GUI() {
+
+        createWindow();
+        createTextArea();
+        createMenuBar();
+        createFileMenu();
+        window.setVisible(true);
+    }
+    public void createWindow() {
+        window = new JFrame("Notepad");
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void createTextArea(){
+
+        textArea = new JTextArea();
+        scrollPane  = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        window.add(scrollPane);
+    }
+
+    public void createMenuBar() {
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+
+    }
+
+    public void createFileMenu(){
+        iNew= new JMenuItem("New");
+        menuFile.add(iNew);
+
+        iOpen= new JMenuItem("Open");
+        menuFile.add(iOpen);
+
+        iSave= new JMenuItem("Save");
+        menuFile.add(iSave);
+
+        iSaveAs= new JMenuItem("Save As");
+        menuFile.add(iSaveAs);
+
+        iExit= new JMenuItem("Exit");
+        menuFile.add(iExit);
+
+    }
 }
