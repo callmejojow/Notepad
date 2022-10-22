30c6517fa8c29c546a94f5440d219ac2c8ed3435
callmejojow <73418982+callmejojow@users.noreply.github.com>
2022-10-22 01:08:40 -0400

    add save and exit functions

--- src/Function_File.java
+++ src/Function_File.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.function.Function;
 
 public class Function_File {
@@ -12,9 +13,10 @@ public class Function_File {
     }
 
     public void newFile() {
-
         gui.textArea.setText("");
         gui.window.setTitle("New");
+        fileName = null;
+        fileAddress = null;
     }
 
     public void open() {
@@ -28,9 +30,54 @@ public class Function_File {
         }
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader());
+            BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+            gui.textArea.setText("");
+            String line = null;
+            while((line = br.readLine()) != null) {
+                gui.textArea.append(line + "\n");
+            }
+            br.close();
         } catch(Exception e){
-
+            System.out.println("FILE NOT OPENED.");
         }
+    }
+
+    public void save(){
+        if(fileName==null) {
+            saveAs();
+        }
+        else {
+            try {
+                FileWriter fw = new FileWriter(fileAddress + fileName);
+                fw.write(gui.textArea.getText());
+                gui.window.setTitle(fileName);
+                fw.close();
+            } catch(Exception e) {
+                System.out.println("Something went wrong");
+            }
+        }
+    }
+
+    public void saveAs(){
+        FileDialog fd = new FileDialog(gui.window, "Save", FileDialog.SAVE);
+        fd.setVisible(true);
+
+        if(fd.getFile() != null){
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+
+        try {
+            FileWriter fw = new FileWriter(fileAddress + fileName);
+            fw.write(gui.textArea.getText());
+            fw.close();
+        } catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+
+    public void exit() {
+        System.exit(0);
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -66,12 +66,18 @@ public class GUI implements ActionListener {
         menuFile.add(iOpen);
 
         iSave= new JMenuItem("Save");
+        iSave.addActionListener(this);
+        iSave.setActionCommand("Save");
         menuFile.add(iSave);
 
         iSaveAs= new JMenuItem("Save As");
+        iSaveAs.addActionListener(this);
+        iSaveAs.setActionCommand("Save As");
         menuFile.add(iSaveAs);
 
         iExit= new JMenuItem("Exit");
+        iExit.addActionListener(this);
+        iExit.setActionCommand("Exit");
         menuFile.add(iExit);
     }
     @Override
@@ -81,6 +87,9 @@ public class GUI implements ActionListener {
         switch(command){
             case "New": file.newFile(); break;
             case "Open": file.open(); break;
+            case "Save": file.save(); break;
+            case "Save As": file.saveAs(); break;
+            case "Exit": file.exit(); break;
         }
     }
 }
